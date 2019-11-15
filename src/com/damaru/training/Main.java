2a146790295eec056ed7fca64ac595503e2b39e9
Michael Davis <michael@damaru.com>
2019-11-15 13:02:46 -0500

    Added stuff

---
+++ src/com/damaru/training/Main.java
@@ -0,0 +1,17 @@
+package com.damaru.training;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        String myString = "text";
+        int i = Integer.valueOf(myString);
+        
+        Stuff s = new Stuff();
+        double val = s.sum();
+        
+        System.out.println(i);
+
+    }
+
+}
---
+++ src/com/damaru/training/Stuff.java
@@ -0,0 +1,11 @@
+package com.damaru.training;
+
+public class Stuff {
+    int i = 5;
+    float x = 4.4f;
+    public int j = 6;
+    
+    public double sum() {
+        return i + x;
+    }
+}
